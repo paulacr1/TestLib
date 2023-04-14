0fb8a1092fd715c7657e338054625555d0fda401
Paula Rosa <paula.rosa@hellofresh.com>
2023-04-14 17:28:08 +0200

    update main activity AGAIN

--- app/src/main/java/com/example/testlib/MainActivity.kt
+++ app/src/main/java/com/example/testlib/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android adding content")
+                    Greeting("Android adding content AGAIN")
                 }
             }
         }
